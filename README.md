823cd272dd727b1b80942cd59cc5d812a84dc405
Maurits van Altvorst <mvanaltvorst@icloud.com>
2024-07-06 23:52:55 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,6 @@
+# Betting Odds Probability Calculator
+
+![An illustration of the interface](./illustration.png)
+
+Simple Flask-based web interface that can be used for football gambling. Allows multiple users to quickly adjust live odds and get implied winning probabilities. 
+
---
+++ app.py
@@ -0,0 +1,63 @@
+from flask import Flask, render_template
+from flask_socketio import SocketIO, emit
+import numpy as np
+import pandas as pd
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'secret!'
+socketio = SocketIO(app)
+
+state = {
+    'team1_name': 'Team 1',
+    'team2_name': 'Team 2',
+    'team1_odds': 6.0,
+    'draw_odds': 3.0,
+    'team2_odds': 2,
+    'team1_advancing_odds': 1.5,
+    'team2_advancing_odds': 2.5
+}
+
+def betting_implied_ask(odds):
+    return 1 / np.array(odds)
+
+def betting_implied_mean(odds):
+    implied_probs = 1 / np.array(odds)
+    return implied_probs / np.sum(implied_probs)
+
+def advancing_probs(team1_advancing_odds, team2_advancing_odds):
+    match_odds = np.array([team1_advancing_odds, float("inf"), team2_advancing_odds])
+    implied_probs = 1 / match_odds
+    return implied_probs / np.sum(implied_probs)
+
+def calculate_probabilities():
+    match_odds = [state['team1_odds'], state['draw_odds'], state['team2_odds']]
+    methods = [
+        ("Implied Ask", betting_implied_ask(match_odds)),
+        ("Implied Mean", betting_implied_mean(match_odds)),
+        ("Implied Advancing Mean", advancing_probs(state['team1_advancing_odds'], state['team2_advancing_odds'])),
+    ]
+    data = []
+    for method, probs in methods:
+        row = [method] + [f"{prob*100:.1f}%" for prob in probs]
+        data.append(row)
+    df = pd.DataFrame(data, columns=["Method", state['team1_name'], "Draw", state['team2_name']])
+    return df.to_dict('records')
+
+def calculate_implied_margin():
+    match_odds = [state['team1_odds'], state['draw_odds'], state['team2_odds']]
+    implied_margin = np.sum(1 / np.array(match_odds)) - 1
+    return f"{implied_margin:.2%}"
+
+@app.route('/')
+def index():
+    return render_template('index.html', state=state)
+
+@socketio.on('update_odds')
+def handle_update_odds(data):
+    state.update(data)
+    probabilities = calculate_probabilities()
+    implied_margin = calculate_implied_margin()
+    emit('odds_updated', {'state': state, 'probabilities': probabilities, 'implied_margin': implied_margin}, broadcast=True)
+
+if __name__ == '__main__':
+    socketio.run(app, debug=True, port=5001)
